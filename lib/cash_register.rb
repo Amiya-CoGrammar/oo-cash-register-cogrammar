3c890bef6627f315ef563a3146dc7753463317ce
CoGrammar Code Review <reviews@cogrammar.com>
2019-09-17 13:36:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
+class CashRegister
 
+  def initilize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price, quantity = 1)
+
+    self.total += price * quantity
+    quantity.times do
+      @items << title
+    end
+
+  end
+
+  def apply_discount
+    if @discount > 0
+      self.total -= @discount * 10
+      return "After the discount, the total comes to #{@total}"
+    else
+      return "There is no discount to apply"
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    self.total -= self.total
+  end
+  
+end
