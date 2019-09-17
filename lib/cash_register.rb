0851a3558068808395f50ddb870da5ecef51dfb7
CoGrammar Code Review <reviews@cogrammar.com>
2019-09-17 13:40:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,25 +1,28 @@
 class CashRegister
+
   attr_accessor :total, :discount
 
-  def initialize(discount = 0)
+  def initilize(discount = 0)
     @total = 0
     @discount = discount
     @items = []
   end
 
   def add_item(title, price, quantity = 1)
+
     self.total += price * quantity
     quantity.times do
       @items << title
     end
+
   end
 
   def apply_discount
     if @discount > 0
       self.total -= @discount * 10
-      return "After the discount, the total comes to $800."
+      return "After the discount, the total comes to #{@total}"
     else
-      return "There is no discount to apply."
+      return "There is no discount to apply"
     end
   end
 
@@ -30,4 +33,5 @@ class CashRegister
   def void_last_transaction
     self.total -= self.total
   end
+  
 end
